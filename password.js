2fc4b38021a03e5b9ea93fc0b6a643cd10831016
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-04-11 19:04:08 +0200

    Make getRandomBytes a local function.

--- password.js
+++ password.js
@@ -1,18 +1,22 @@
-/* Public domain / CC0 */
+/*
+ * Written by @dchest
+ * Public domain / CC0
+ */
 
-function getRandomBytes(length) {
-  var words = sjcl.random.randomWords(Math.ceil(length / 4), 0);
-  var bytes = [];
-  for (var i = 0; i < words.length; i++) {
-    bytes.push((words[i] >>>  0) & 0xff);
-    bytes.push((words[i] >>>  8) & 0xff);
-    bytes.push((words[i] >>> 16) & 0xff);
-    bytes.push((words[i] >>> 24) & 0xff);
-  }
-  return bytes.slice(0, length);
-}
- 
 function generatePassword(length) {
+
+  function getRandomBytes(length) {
+    var words = sjcl.random.randomWords(Math.ceil(length / 4), 0);
+    var bytes = [];
+    for (var i = 0; i < words.length; i++) {
+      bytes.push((words[i] >>>  0) & 0xff);
+      bytes.push((words[i] >>>  8) & 0xff);
+      bytes.push((words[i] >>> 16) & 0xff);
+      bytes.push((words[i] >>> 24) & 0xff);
+    }
+    return bytes.slice(0, length);
+  }
+ 
   // To avoid modulo bias, alphabet length must conform to 256 % length == 0, e.g. 64.
   var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@'.split('');
   var bytes = getRandomBytes(length);
